df98ebad0e0f200ea332ce35812632979cfdae90
Delan Gono <30848798+SteiKnight@users.noreply.github.com>
2024-12-28 22:09:18 +0100

    modified: finalized created of database and all its crud functions as well as connected state management provider

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -1,7 +1,60 @@
-class NoteDatabase {
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:notesdbisar/models/note.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NoteDatabase extends ChangeNotifier {
+  static late Isar isar;
+
   //INITIALIZE DATA
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [NoteSchema],
+      directory: dir.path,
+    );
+  }
+
+  //list of notes
+  final List<Note> currentNotes = [];
+
   //CREATE
+  Future<void> addNote(String textFromUser) async {
+    //create a new note object
+    final newNote = Note()..text = textFromUser;
+
+    //save note to database
+    await isar.writeTxn(() => isar.notes.put(newNote));
+
+    //reread from database
+    await fetchNotes();
+  }
+
   //READ
+  Future<void> fetchNotes() async {
+    List<Note> fetchedNotes = await isar.notes.where().findAll();
+    currentNotes.clear();
+    currentNotes.addAll(fetchedNotes);
+    notifyListeners();
+  }
+
   //UPDATE
+  Future<void> updateNote(int id, String newText) async {
+    final existingNote = await isar.notes.get(id);
+    existingNote != null
+        ? {
+            existingNote.text = newText,
+            await isar.writeTxn(
+              () => isar.notes.put(existingNote),
+            ),
+            await fetchNotes(),
+          }
+        : {};
+  }
+
   //DELETE
+  Future<void> deleteNote(int id) async {
+    await isar.writeTxn(() => isar.notes.delete(id));
+    await fetchNotes();
+  }
 }
